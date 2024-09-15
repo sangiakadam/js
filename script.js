1077249a81040ffb29150998bb20c9f755fca5d5
sangiakadam <sangitakadam118@proton.me>
2024-09-15 09:04:25 +0000

    if,else,switch

---
+++ script.js
@@ -0,0 +1,141 @@
+// const a=[[ 1,2,3],
+//         [4,5]]
+// console.log(a[1][1]);
+
+// [].concat
+
+// function a(string){
+//     const inputarr=string.split("");
+//     console.log(inputarr);
+//     inputarr.reverse();
+//     const reversestr=inputarr.join("");
+//     return reversestr;
+// }
+// const output=a("sangita");
+// console.log(output);
+
+// step 1 : covert number to Array
+// // step 1 : covert number to Array
+
+// function a(number){
+//     const inputarr=string(number);
+//     console.log(inputarr.map(number));
+//     inputarr.reverse();
+//     const reversestr=inputarr.join("");
+//     return reversestr;
+// }
+// const output=a(123);
+// console.log(output);
+
+
+//  const fruits= "mango orenge pineapple graphs banana".split(",");
+//  console.log(fruits);
+// for(let i=0; i<fruits.length;i++);
+// {
+//     console.log(fruits[i]);
+    
+// // }
+
+
+// const fruit=["apple","banana","kiwi","pineapple"]
+// console.log(fruit);
+// fruit.splice(1,2,"orenge");
+// console.log(fruit);
+
+// const day=Number(prompt("ENTER DAY NAME"));
+// if (day==1);
+// console.log(monday);
+// if (day==2);
+// console.log(tue);
+// if (day==1);
+// console.log(monday);
+// if (day==1);
+// console.log(monday);
+// if (day==1);
+// console.log(monday);
+// if (day==1);
+// console.log(monday);
+// if (day==1);
+// console.log(monday);
+// if (day==1);
+// console.log(monday); 
+
+
+
+// const day=Number(prompt("ENTER DAY NAME"));
+// switch (day){
+//         case 1:
+//         console.log("mondat");
+//         break;
+//         case 2:
+//             console.log("tue");
+//             break;
+//         case 3:
+//             console.log("wen");
+//             break;
+//         case 4:
+//             console.log("tru");
+//             break;
+//         case 5:
+//             console.log("fri");
+//             break;
+//         case 6:
+//                 console.log("satu");   
+//                 break   ;  
+//         case 7:
+//             console.log("sun");
+//             break
+//         default:
+//             console.log(invalid );
+            
+
+        
+// }
+const month=Number(prompt("ENTER month name"));
+switch (month){
+        case 1:
+        console.log("jan");
+        break;
+        case 2:
+            console.log("feb");
+            break;
+        case 3:
+            console.log("march");
+            break;
+        case 4:
+            console.log("april");
+            break;
+        case 5:
+            console.log("may");
+            break;
+        case 6:
+                console.log("jun");   
+                break   ;  
+        case 7:
+            console.log("july");
+            break;
+        case 8:
+            console.log("aug");
+            break;
+        case 9:
+            console.log("sept");
+            break;
+        case 10:
+            console.log("oct");
+            break;
+        case 11:
+            console.log("nov");
+            break;
+        case 12:
+            console.log("dic");
+            break;
+        
+
+
+        default:
+            console.log(invalid  );
+            
+
+        
+}
+
